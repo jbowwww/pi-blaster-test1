1b92a603292f4f4b5187ba21f5c480874abc1740
jbowwww <jbowwww@gmail.com>
2019-09-22 08:48:01 +0800

    Separate colours

--- index.js
+++ index.js
@@ -9,26 +9,25 @@ try {
 
 
 
-	let p = 0, dir = 0;	
+	let i = 0, pArr = [0, 0, 0], dir = 0;	
 	while(1) {
 	await delay(1000);
-
+	let p = pArr[i];
 	for (; p <= 1 && p >= 0; p += (dir === 0 ? +0.05 : -0.05)) {
-		piblaster.setPwm(17, p);
-		piblaster.setPwm(22, p);	
-		piblaster.setPwm(27, p);
+		piblaster.setPwm([17, 22, 27][i], p);
 		delay(1);
 	}
-
-	await delay(1000);
-	if (p >= 0.945) {
+	pArr[i] = p;
+	i = ++i % 3;
+	await delay(400);
+	if (pArr[i] >= 0.945) {
 		dir = 1;
-		p = 1;
+		pArr[i] = 1;
 	} else {
 		dir = 0;
-		p = 0;
+		pArr[i] = 0;
 	}
-	console.log(`p = ${p} , going ${dir === 1 ? 'down' : 'up'}`);
+	console.log(`i = ${i} p = ${p} , pArr=${pArr} going ${dir === 1 ? 'down' : 'up'}`);
 }
 
 	piblaster.setPwm(17, 0);
